327f87d9b20b138860000f4b4d45ab7e5b50acab
Ângelo Patricio Soares <a.silveira@xsensors.ai>
2021-08-05 22:11:53 -0300

    initial commit

---
+++ 06-INTEGRACOES/src/index.js
@@ -0,0 +1,9 @@
+import Heading from "./components/heading/heading";
+
+const heading = new Heading();
+
+heading.create("pagina principal");
+
+console.log(VERSION);
+console.log(PORT);
+console.log(process.env.API_KEY);
